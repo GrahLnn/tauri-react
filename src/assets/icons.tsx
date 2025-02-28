7a14632d8ad3fbc4fc2111300ef2906c0268f4f6
GrahLnn <grahlnn@lingusic.com>
2025-02-28 11:06:35 +0800

    update topbar

--- src/assets/icons.tsx
+++ src/assets/icons.tsx
@@ -100,4 +100,312 @@ export const icons = {
       </svg>
     );
   },
+  pin({ size, color, className }: IconProps) {
+    return (
+      // biome-ignore lint/a11y/noSvgWithoutTitle: <explanation>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={size || 18}
+        height={size || 18}
+        viewBox="0 0 18 18"
+        className={className}
+      >
+        <g
+          fill="none"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="1.5"
+          stroke={color || "currentColor"}
+        >
+          <path
+            d="M10.371 15.553C10.803 14.996 11.391 14.083 11.719 12.835C11.888 12.193 11.949 11.611 11.962 11.134L14.967 8.129C15.748 7.348 15.748 6.082 14.967 5.301L12.699 3.033C11.918 2.252 10.652 2.252 9.87101 3.033L6.86601 6.038C6.38801 6.051 5.80701 6.112 5.16501 6.281C3.91701 6.609 3.00401 7.197 2.44701 7.629L10.372 15.554L10.371 15.553Z"
+            fill={color || "currentColor"}
+            fillOpacity="0.3"
+            data-stroke="none"
+            stroke="none"
+          />
+          <path d="M3.08099 14.919L6.40899 11.591" />
+          <path d="M10.371 15.553C10.803 14.996 11.391 14.083 11.719 12.835C11.888 12.193 11.949 11.611 11.962 11.134L14.967 8.129C15.748 7.348 15.748 6.082 14.967 5.301L12.699 3.033C11.918 2.252 10.652 2.252 9.87101 3.033L6.86601 6.038C6.38801 6.051 5.80701 6.112 5.16501 6.281C3.91701 6.609 3.00401 7.197 2.44701 7.629L10.372 15.554L10.371 15.553Z" />
+        </g>
+      </svg>
+    );
+  },
+  lang({ size, color, className }: IconProps) {
+    return (
+      // biome-ignore lint/a11y/noSvgWithoutTitle: <explanation>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={size || 18}
+        height={size || 18}
+        viewBox="0 0 18 18"
+        className={className}
+      >
+        <g
+          fill="none"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="1.5"
+          stroke={color || "currentColor"}
+        >
+          <path d="M2.25 4.25H10.25" /> <path d="M6.25 2.25V4.25" />
+          <path d="M4.25 4.25C4.341 6.926 6.166 9.231 8.75 9.934" />
+          <path d="M8.25 4.25C7.85 9.875 2.25 10.25 2.25 10.25" />
+          <path d="M9.25 15.75L12.25 7.75H12.75L15.75 15.75" />
+          <path d="M10.188 13.25H14.813" />
+        </g>
+      </svg>
+    );
+  },
+  sliders({ size, color, className }: IconProps) {
+    return (
+      // biome-ignore lint/a11y/noSvgWithoutTitle: <explanation>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={size || 18}
+        height={size || 18}
+        viewBox="0 0 18 18"
+        className={className}
+      >
+        <g
+          fill="none"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="1.5"
+          stroke={color || "currentColor"}
+        >
+          <line x1="13.25" y1="5.25" x2="16.25" y2="5.25" />
+          <line x1="1.75" y1="5.25" x2="8.75" y2="5.25" />
+          <circle cx="11" cy="5.25" r="2.25" />
+          <line x1="4.75" y1="12.75" x2="1.75" y2="12.75" />
+          <line x1="16.25" y1="12.75" x2="9.25" y2="12.75" />
+          <circle cx="7" cy="12.75" r="2.25" />
+        </g>
+      </svg>
+    );
+  },
+  sliders2({ size, color, className }: IconProps) {
+    return (
+      // biome-ignore lint/a11y/noSvgWithoutTitle: <explanation>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={size || 18}
+        height={size || 18}
+        viewBox="0 0 18 18"
+        className={className}
+      >
+        <g
+          fill="none"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="1.5"
+          stroke={color || "currentColor"}
+        >
+          <line x1="15.25" y1="9" x2="16.25" y2="9" />
+          <line x1="1.75" y1="9" x2="9" y2="9" />
+          <line x1="5" y1="3.75" x2="1.75" y2="3.75" />
+          <line x1="16.25" y1="3.75" x2="11.25" y2="3.75" />
+          <line x1="5" y1="14.25" x2="1.75" y2="14.25" />
+          <line x1="16.25" y1="14.25" x2="11.25" y2="14.25" />
+          <circle cx="11" cy="9" r="1.75" />
+          <circle cx="6.75" cy="3.75" r="1.75" />
+          <circle cx="6.75" cy="14.25" r="1.75" />
+        </g>
+      </svg>
+    );
+  },
+  barsFilter({ size, color, className }: IconProps) {
+    return (
+      // biome-ignore lint/a11y/noSvgWithoutTitle: <explanation>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={size || 18}
+        height={size || 18}
+        viewBox="0 0 18 18"
+        className={className}
+      >
+        <g
+          fill="none"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="1.5"
+          stroke={color || "currentColor"}
+        >
+          <line x1="5.25" y1="9" x2="12.75" y2="9" />
+          <line x1="2.75" y1="4.25" x2="15.25" y2="4.25" />
+          <line x1="8" y1="13.75" x2="10" y2="13.75" />
+        </g>
+      </svg>
+    );
+  },
+  globe3({ size, color, className }: IconProps) {
+    return (
+      // biome-ignore lint/a11y/noSvgWithoutTitle: <explanation>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={size || 18}
+        height={size || 18}
+        viewBox="0 0 18 18"
+        className={className}
+      >
+        <g
+          fill="none"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="1.5"
+          stroke={color || "currentColor"}
+        >
+          <ellipse cx="9" cy="9" rx="3" ry="7.25" />
+          <line x1="2.106" y1="6.75" x2="15.894" y2="6.75" />
+          <line x1="2.29" y1="11.75" x2="15.71" y2="11.75" />
+          <circle cx="9" cy="9" r="7.25" />
+        </g>
+      </svg>
+    );
+  },
+  cloudRefresh({ size, color, className }: IconProps) {
+    return (
+      // biome-ignore lint/a11y/noSvgWithoutTitle: <explanation>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={size || 18}
+        height={size || 18}
+        viewBox="0 0 18 18"
+        className={className}
+      >
+        <g
+          fill="none"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="1.5"
+          stroke={color || "currentColor"}
+        >
+          <path d="M14.24,13.823c1.195-.627,2.01-1.88,2.01-3.323,0-1.736-1.185-3.182-2.786-3.609-.186-2.314-2.102-4.141-4.464-4.141-2.485,0-4.5,2.015-4.5,4.5,0,.35,.049,.686,.124,1.013-1.597,.067-2.874,1.374-2.874,2.987,0,1.306,.835,2.417,2,2.829" />
+          <polyline points="9.25 13.75 11.75 13.75 11.75 11.25" />
+          <path d="M11,16.387c-.501,.531-1.212,.863-2,.863-1.519,0-2.75-1.231-2.75-2.75s1.231-2.75,2.75-2.75c1.166,0,2.162,.726,2.563,1.75" />
+        </g>
+      </svg>
+    );
+  },
+  magnifler3({ size, color, className }: IconProps) {
+    return (
+      // biome-ignore lint/a11y/noSvgWithoutTitle: <explanation>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={size || 18}
+        height={size || 18}
+        viewBox="0 0 18 18"
+        className={className}
+      >
+        <g
+          fill="none"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="1.5"
+          stroke={color || "currentColor"}
+        >
+          <line x1="15.25" y1="15.25" x2="11.285" y2="11.285" />
+          <circle cx="7.75" cy="7.75" r="5" />
+          <path d="M7.75,5.25c1.381,0,2.5,1.119,2.5,2.5" />
+        </g>
+      </svg>
+    );
+  },
+  menuBars({ size, color, className }: IconProps) {
+    return (
+      // biome-ignore lint/a11y/noSvgWithoutTitle: <explanation>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={size || 18}
+        height={size || 18}
+        viewBox="0 0 18 18"
+        className={className}
+      >
+        <g
+          fill="none"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="1.5"
+          stroke={color || "currentColor"}
+        >
+          <line x1="5.75" y1="9" x2="16.25" y2="9" />
+          <line x1="1.75" y1="9" x2="2.75" y2="9" />
+          <line x1="15.25" y1="3.75" x2="16.25" y2="3.75" />
+          <line x1="1.75" y1="3.75" x2="12.25" y2="3.75" />
+          <line x1="15.25" y1="14.25" x2="16.25" y2="14.25" />
+          <line x1="1.75" y1="14.25" x2="12.25" y2="14.25" />
+        </g>
+      </svg>
+    );
+  },
+  tableCols2({ size, color, className }: IconProps) {
+    return (
+      // biome-ignore lint/a11y/noSvgWithoutTitle: <explanation>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={size || 18}
+        height={size || 18}
+        viewBox="0 0 18 18"
+        className={className}
+      >
+        <g
+          fill="none"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="1.5"
+          stroke={color || "currentColor"}
+        >
+          <line x1="9" y1="2.75" x2="9" y2="15.25" />
+          <rect x="2.75" y="2.75" width="12.5" height="12.5" rx="2" ry="2" />
+        </g>
+      </svg>
+    );
+  },
+  gridCircle({ size, color, className }: IconProps) {
+    return (
+      // biome-ignore lint/a11y/noSvgWithoutTitle: <explanation>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={size || 18}
+        height={size || 18}
+        viewBox="0 0 18 18"
+        className={className}
+      >
+        <g
+          fill="none"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="1.5"
+          stroke={color || "currentColor"}
+        >
+          <circle cx="5" cy="5" r="2.5" />
+          <circle cx="13" cy="5" r="2.5" />
+          <circle cx="5" cy="13" r="2.5" />
+          <circle cx="13" cy="13" r="2.5" />
+        </g>
+      </svg>
+    );
+  },
+  arrowDown({ size, color, className }: IconProps) {
+    return (
+      // biome-ignore lint/a11y/noSvgWithoutTitle: <explanation>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={size || 18}
+        height={size || 18}
+        viewBox="0 0 18 18"
+        className={className}
+      >
+        <g
+          fill="none"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="1.5"
+          stroke={color || "currentColor"}
+        >
+          <line x1="9" y1="15.25" x2="9" y2="2.75" />
+          <polyline points="13.25 11 9 15.25 4.75 11" />
+        </g>
+      </svg>
+    );
+  },
 };
--- src/topbar.tsx
+++ src/topbar.tsx
@@ -7,12 +7,13 @@ import {
   useCallback,
   useEffect,
   useState,
+  type KeyboardEvent,
 } from "react";
 
 const appWindow = new Window("main");
 const os = platform();
 
-interface TopBarButtonProps extends PropsWithChildren {
+interface WindowsButtonProps extends PropsWithChildren {
   onClick?: () => void;
   className?: string;
   title?: string;
@@ -20,15 +21,15 @@ interface TopBarButtonProps extends PropsWithChildren {
   emphasizeColor?: string;
 }
 
-function TopBarButton({
+function WindowsButton({
   children,
   onClick,
   title,
   color,
   className,
   emphasizeColor,
-}: TopBarButtonProps) {
-  const handleKeyDown = (event: React.KeyboardEvent) => {
+}: WindowsButtonProps) {
+  const handleKeyDown = (event: KeyboardEvent<HTMLButtonElement>) => {
     if (event.key === "Enter" || event.key === " ") {
       onClick?.();
     }
@@ -41,7 +42,7 @@ function TopBarButton({
       onKeyDown={handleKeyDown}
       className={cn(
         "h-8 w-[46px] flex items-center justify-center",
-        "opacity-30 hover:opacity-100",
+        "opacity-60 hover:opacity-100",
         "transition-all",
         color
           ? "hover:bg-[var(--hover-bg-color)] hover:text-[var(--hover-text-color)]"
@@ -85,23 +86,57 @@ function WindowsControls() {
 
   return (
     <div className="flex items-center">
-      <TopBarButton onClick={() => appWindow.minimize()}>
+      <WindowsButton onClick={() => appWindow.minimize()}>
         <icons.minus size={14} />
-      </TopBarButton>
-      <TopBarButton onClick={() => appWindow.toggleMaximize()}>
+      </WindowsButton>
+      <WindowsButton onClick={() => appWindow.toggleMaximize()}>
         {maximized ? (
           <icons.stacksquare size={14} />
         ) : (
           <icons.square size={14} />
         )}
-      </TopBarButton>
-      <TopBarButton
+      </WindowsButton>
+      <WindowsButton
         onClick={() => appWindow.close()}
         color="#e81123"
         emphasizeColor="#f0f0f0"
       >
         <icons.xmark />
-      </TopBarButton>
+      </WindowsButton>
+    </div>
+  );
+}
+
+interface CtrlButtonProps {
+  children: React.ReactNode;
+  onClick?: () => void;
+  className?: string;
+  o?: string;
+  p?: string;
+}
+
+function CtrlButton({
+  children,
+  onClick = () => {},
+  className,
+  o,
+  p,
+}: CtrlButtonProps) {
+  return (
+    <div data-tauri-drag-region="false">
+      {/* biome-ignore lint/a11y/useKeyWithClickEvents: <explanation> */}
+      <div
+        className={cn([
+          "rounded-md cursor-default",
+          p || "p-2",
+          o || "opacity-60",
+          "hover:bg-black/5 dark:hover:bg-white/5 hover:opacity-100 transition-all duration-300 ease-in-out",
+          className,
+        ])}
+        onClick={onClick}
+      >
+        {children}
+      </div>
     </div>
   );
 }
@@ -117,22 +152,90 @@ function LeftControls() {
 function RightControls() {
   return (
     <div className="flex items-center">
+      <CtrlButton>
+        <icons.magnifler3 size={14} />
+      </CtrlButton>
+      <CtrlButton>
+        <icons.globe3 size={14} />
+      </CtrlButton>
+
+      <CtrlButton>
+        <icons.arrowDown size={14} />
+      </CtrlButton>
+
       <WindowsControls />
     </div>
   );
 }
 
+function MiddleControls() {
+  return (
+    <div className={cn(["flex items-center h-full"])}>
+      <CtrlButton>
+        <icons.gridCircle size={14} />
+      </CtrlButton>
+
+      <CtrlButton className="text-xs font-light" o="opacity-80" p="py-2 px-5">
+        <span>A useful app</span>
+      </CtrlButton>
+      <CtrlButton>
+        <icons.sliders size={14} />
+      </CtrlButton>
+    </div>
+  );
+}
+
 export default function TopBar() {
+  const [windowFocused, setWindowFocused] = useState(true);
+
+  useEffect(() => {
+    const handleFocus = () => setWindowFocused(true);
+    const handleBlur = () => setWindowFocused(false);
+    window.addEventListener("focus", handleFocus);
+    window.addEventListener("blur", handleBlur);
+    return () => {
+      window.removeEventListener("focus", handleFocus);
+      window.removeEventListener("blur", handleBlur);
+    };
+  }, []);
+
   return (
     <>
       {os === "windows" && (
-        <div
-          className="flex justify-between w-screen h-8"
+        <header
+          className={cn([
+            "absolute top-0 left-0",
+            "w-screen h-8 z-100 select-none",
+            "before:absolute before:inset-0 before:-z-10",
+            "before:bg-gradient-to-b before:from-[var(--app-bg)] before:to-[var(--app-bg)]/60",
+            "after:absolute after:inset-0 after:-z-10",
+            "after:backdrop-blur-[16px] after:opacity-100 after:origin-top",
+            "after:bg-gradient-to-b after:from-transparent after:via-transparent after:to-white/0",
+            "after:mask-image-[linear-gradient(to_bottom,rgba(0,0,0,1)_0%,rgba(0,0,0,0)_100%)]",
+          ])}
           data-tauri-drag-region
         >
-          <LeftControls />
-          <RightControls />
-        </div>
+          <div
+            className={cn([
+              "grid grid-cols-[1fr_auto_1fr] w-full h-full",
+              !windowFocused && "opacity-30",
+              "transition-all duration-300",
+            ])}
+          >
+            <div
+              data-tauri-drag-region="false"
+              className="flex justify-start pl-1"
+            >
+              <LeftControls />
+            </div>
+            <div data-tauri-drag-region="false">
+              <MiddleControls />
+            </div>
+            <div data-tauri-drag-region="false" className="flex justify-end">
+              <RightControls />
+            </div>
+          </div>
+        </header>
       )}
     </>
   );
