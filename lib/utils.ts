40f6e1e7c534fb0f666e6ab7576fe1f07589df42
GrahLnn <liuyvn19@gmail.com>
2025-09-23 19:27:18 +0800

    o

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,15 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { me } from "./matchable";
+import { platform } from "@tauri-apps/plugin-os";
+
+export const app_state = me(!import.meta.env.DEV ? "pub" : "dev");
+export const os = me(platform());
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function up1st(str: string) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
 }
