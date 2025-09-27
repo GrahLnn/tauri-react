7302d1542a890ea75aeb95835a21d47c73f5105c
GrahLnn <liuyvn19@gmail.com>
2025-09-27 13:22:48 +0800

    o

--- src/state_machine/templete/api.ts
+++ src/state_machine/templete/api.ts
@@ -4,8 +4,11 @@ import { useSelector } from "@xstate/react";
 import { me } from "@/lib/matchable";
 import { MainStateT, payloads, sig } from "./events";
 import { B } from "@/lib/comb";
+import { createSender } from "../kit";
 
 export const actor = createActor(machine);
+const send = createSender(actor);
+
 export const hook = {
   useState: () => useSelector(actor, (shot) => me(shot.value as MainStateT)),
   useContext: () => useSelector(actor, (shot) => shot.context),
